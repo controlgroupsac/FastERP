eabe9a5e30b832c31bd90ff2e6a9f2353765d43c
Heiner <heineraes@gmail.com>
2014-12-27 03:09:33 -0500

    Esqueleto html, CSS y JS para homepage.html. Tambien se incluyeron todos los icons en homepage e index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-
+$(function () {
+	$("#proveedor").hover(function(){
+		$("#proveedorDetalle").addClass("block");
+		$("#ordenDeCompraDetalle").removeClass("block");
+		$("#inventarioDetalle").removeClass("block");
+		$("#inventarioDetalle2").removeClass("block");
+		$("#ordenDeVentaDetalle").removeClass("block");
+		$("#clienteDetalle").removeClass("block");
+	})
+	$("#ordenDeCompra").hover(function(){
+		$("#proveedorDetalle").removeClass("block");
+		$("#ordenDeCompraDetalle").addClass("block");
+		$("#inventarioDetalle").removeClass("block");
+		$("#inventarioDetalle2").removeClass("block");
+		$("#ordenDeVentaDetalle").removeClass("block");
+		$("#clienteDetalle").removeClass("block");
+	})
+	$("#inventario").hover(function(){
+		$("#proveedorDetalle").removeClass("block");
+		$("#ordenDeCompraDetalle").removeClass("block");
+		$("#inventarioDetalle").addClass("block");
+		$("#inventarioDetalle2").addClass("block");
+		$("#ordenDeVentaDetalle").removeClass("block");
+		$("#clienteDetalle").removeClass("block");
+	})
+	$("#ordenDeVenta").hover(function(){
+		$("#proveedorDetalle").removeClass("block");
+		$("#ordenDeCompraDetalle").removeClass("block");
+		$("#inventarioDetalle").removeClass("block");
+		$("#inventarioDetalle2").removeClass("block");
+		$("#ordenDeVentaDetalle").addClass("block");
+		$("#clienteDetalle").removeClass("block");
+	})
+	$("#cliente").hover(function(){
+		$("#proveedorDetalle").removeClass("block");
+		$("#ordenDeCompraDetalle").removeClass("block");
+		$("#inventarioDetalle").removeClass("block");
+		$("#inventarioDetalle2").removeClass("block");
+		$("#ordenDeVentaDetalle").removeClass("block");
+		$("#clienteDetalle").addClass("block");
+	})
+})
